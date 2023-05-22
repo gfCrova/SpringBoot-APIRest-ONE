0557b220997a55c04073f44c4bcb26b2b6e1176f
gfCrova <giancrova.cl5@gmail.com>
2023-05-21 21:46:34 -0300

    add record datos de direccion

--- src/main/java/med/voll/api/direccion/DatosDeDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDeDireccion.java
@@ -1,9 +1,15 @@
 package med.voll.api.direccion;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DatosDeDireccion(
+        @NotBlank
         String calle,
+        @NotBlank
         String numero,
+        @NotBlank
         String distrito,
+        @NotBlank
         String ciudad
 ) {
 }
