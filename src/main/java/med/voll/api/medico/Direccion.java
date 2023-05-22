c3c3d0e10bcdd0e32c08ea30c0a841e358268214
gfCrova <giancrova.cl5@gmail.com>
2023-05-21 21:41:51 -0300

    entidad direccion y medico agregando métodos con lombok

--- src/main/java/med/voll/api/medico/Direccion.java
+++ src/main/java/med/voll/api/medico/Direccion.java
@@ -1,2 +1,26 @@
-package med.voll.api.medico;public class Direccion {
+package med.voll.api.medico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.direccion.DatosDeDireccion;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+
+    private String calle;
+    private String numero;
+    private String distrito;
+    private String ciudad;
+
+    public Direccion(DatosDeDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+    }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,2 +1,38 @@
-package med.voll.api.medico;public class Medico {
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor // Constructor Vacío
+@AllArgsConstructor // Constructor con Todos los Parámetros
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String dni;
+    private String telefono;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+
+
+    public Medico(DatosDeRegistroMedico registro) {
+        this.nombre = registro.nombre();
+        this.email = registro.email();
+        this.dni = registro.dni();
+        this.telefono = registro.telefono();
+        this.especialidad = registro.especialidad();
+        this.direccion = new Direccion(registro.direccion());
+    }
 }
