82d0655341a67df4f0e1957f2567315d76c66470
gfCrova <giancrova.cl5@gmail.com>
2023-05-22 22:41:23 -0300

    modified methods controller medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,17 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosDeRegistroMedico;
-import med.voll.api.medico.DatosListadoMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.DTO.medico.DatosActualizarMedico;
+import med.voll.api.DTO.medico.DatosDeRegistroMedico;
+import med.voll.api.DTO.medico.DatosListadoMedico;
+import med.voll.api.DTO.Medico;
+import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -42,7 +42,9 @@ public class MedicoController {
     }
 
     @PutMapping
-    public void actualizarMedico() {
-
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizar) {
+        Medico medico = medicoRepository.getReferenceById(datosActualizar.id());
+        medico.actualizarDatos(datosActualizar);
     }
 }
