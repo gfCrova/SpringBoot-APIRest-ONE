7bd2803b2056790d723bb8aeb459701addd7ce2e
gfCrova <giancrova.cl5@gmail.com>
2023-05-24 00:35:10 -0300

    add atributos dto respuesta

--- src/main/java/med/voll/api/DTO/medico/DatosRespuestaMedico.java
+++ src/main/java/med/voll/api/DTO/medico/DatosRespuestaMedico.java
@@ -1,2 +1,6 @@
-package med.voll.api.DTO.medico;public record DatosRespuestaMedico() {
+package med.voll.api.DTO.medico;
+
+import med.voll.api.DTO.direccion.DatosDeDireccion;
+
+public record DatosRespuestaMedico(Long id, String nombre, String email, String dni, String telefono, DatosDeDireccion direccion) {
 }
