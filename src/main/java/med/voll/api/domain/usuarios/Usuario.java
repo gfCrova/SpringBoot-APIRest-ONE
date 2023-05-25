94b0af69a62ab6ba4ad8b95f09501fa7adbbdb36
gfCrova <giancrova.cl5@gmail.com>
2023-05-25 18:04:43 -0300

    add entity usuarios

---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String clave;
+
+}
