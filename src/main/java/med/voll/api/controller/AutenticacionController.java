7b466f088df3c0d0636a3f7fea2d84d01582331f
gfCrova <giancrova.cl5@gmail.com>
2023-05-27 15:44:14 -0300

    modificando el método realizarLogin(), retornando dto token

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -3,13 +3,12 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
 import med.voll.api.domain.usuarios.Usuario;
-import med.voll.api.infra.security.DatosJKTtoken;
+import med.voll.api.infra.security.DatosJWTtoken;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,16 +16,19 @@ import org.springframework.web.bind.annotation.*;
 public class AutenticacionController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private AuthenticationManager manager;
 
     @Autowired
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario) {
-        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
-        var usuarioAutenticado = authenticationManager.authenticate(authToken);
-        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
-        return ResponseEntity.ok(new DatosJKTtoken(JWTtoken));
+    public ResponseEntity  realizarLogin(@RequestBody @Valid DatosAutenticacionUsuario datos) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.clave());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.generarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DatosJWTtoken(tokenJWT));
     }
+
 }
