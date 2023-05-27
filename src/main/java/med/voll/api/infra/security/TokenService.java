eea585e37ca1bf67ee4ade2881541acd8f38c2d2
gfCrova <giancrova.cl5@gmail.com>
2023-05-27 15:47:39 -0300

    token service with methods generarToken() and getSubject()

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,24 +15,38 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    @Value("${api.security.secret}")
-    private String apiSecret;
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String generarToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("voll med")
+                    .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
-                    .withClaim("id", usuario.getId())
-                    .withExpiresAt(generarFechaExpiracion())
-                    .sign(algorithm);
+                    .withExpiresAt(fechaExpiracion())
+                    .sign(algoritmo);
         } catch (JWTCreationException exception){
-            throw new RuntimeException(exception);
+            throw new RuntimeException("Error al generar el token JWT", exception);
         }
     }
 
-    private Instant generarFechaExpiracion() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido o expirado!");
+        }
     }
+
+    private Instant fechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
 }
 
