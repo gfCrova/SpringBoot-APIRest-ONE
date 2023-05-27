9a4659fc13bcc61b6cb64adff224d84da4126123
gfCrova <giancrova.cl5@gmail.com>
2023-05-27 15:46:16 -0300

    new SecurityFilter get header Authorization

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,2 +1,36 @@
-package med.voll.api.infra.security;public class SecurityFilter {
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("El filtro esta siendo llamado");
+        var token = request.getHeader("Authorization");
+
+        if (token == "" || token == null) {
+            throw new RuntimeException("El token enviado no es válido");
+        }
+        token = token.replace("Bearer", "");
+
+        System.out.println(token);
+        System.out.println(tokenService.getSubject(token));
+
+
+
+        filterChain.doFilter(request, response);
+    }
 }
