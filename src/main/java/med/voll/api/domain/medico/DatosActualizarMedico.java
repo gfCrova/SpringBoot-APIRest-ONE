3586b8f27eca89c2a256190e6639c48a68ed4350
gfCrova <giancrova.cl5@gmail.com>
2023-05-31 19:51:44 -0300

    reformat code

--- src/main/java/med/voll/api/domain/medico/DatosActualizarMedico.java
+++ src/main/java/med/voll/api/domain/medico/DatosActualizarMedico.java
@@ -3,6 +3,10 @@ package med.voll.api.domain.medico;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.direccion.DatosDeDireccion;
 
-public record DatosActualizarMedico(@NotNull Long id, String nombre, String dni, DatosDeDireccion direccion) {
+public record DatosActualizarMedico(
+        @NotNull Long id,
+        String nombre,
+        String dni,
+        DatosDeDireccion direccion) {
 
 }
--- src/main/java/med/voll/api/domain/medico/DatosRespuestaMedico.java
+++ src/main/java/med/voll/api/domain/medico/DatosRespuestaMedico.java
@@ -2,5 +2,11 @@ package med.voll.api.domain.medico;
 
 import med.voll.api.domain.direccion.DatosDeDireccion;
 
-public record DatosRespuestaMedico(Long id, String nombre, String email, String dni, String telefono, DatosDeDireccion direccion) {
+public record DatosRespuestaMedico(
+        Long id,
+        String nombre,
+        String email,
+        String dni,
+        String telefono,
+        DatosDeDireccion direccion) {
 }
